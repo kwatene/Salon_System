bcd280f60d9cf431e4c839e332e2ff08f9eb7cbf
kimwatene@gmail.com <kimwatene@gmail.com>
2023-05-25 10:57:34 +1200

    Add ServicePack Model Properties

--- Salon_System/Models/ServicePack.cs
+++ Salon_System/Models/ServicePack.cs
@@ -2,5 +2,12 @@
 {
     public class ServicePack
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public ServicePack()
+        {
+        }
     }
 }
