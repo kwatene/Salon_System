bda0be2c3535320540b441494488fb765bf19a36
kimwatene@gmail.com <kimwatene@gmail.com>
2023-05-28 01:52:16 +1200

    Client Module

--- Salon_System/Controllers/ClientController.cs
+++ Salon_System/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Salon_System.Data;
 using Salon_System.Models;
 using System.Text.RegularExpressions;
@@ -131,7 +132,7 @@ namespace Salon_System.Controllers
                             };
                             _context.Client.Add(newClient);                             //Add client record to database
                             _context.SaveChanges();                                         //Save changes made to database
-                            TempData["successMessage"] = "Client created successfully";     //Display success message
+                            TempData["successMessage"] = "New Client Added";     //Display success message
                             return RedirectToAction("Index");                               //Redirect user to Client list
                         }
                         else
@@ -172,7 +173,7 @@ namespace Salon_System.Controllers
             {
                 var record = _context.Client.SingleOrDefault(x => x.Id == id); //Find client record in database by id
 
-                if (record != null) //if found
+                if (record != null) //If client record found
                 {
                     var client = new Client() //Add details to client object
                     {
@@ -192,9 +193,9 @@ namespace Salon_System.Controllers
                     return View(client); //Display client details
                 }
                 else
-                {
-                    TempData["errorMessage"] = $"Client details are not available with ID: {id}"; //If not found - Display message
-                    return RedirectToAction("Index"); //Redirect to Client list
+                {                                                //If not found - Display error message
+                    TempData["errorMessage"] = $"Client details are not available with ID: {id}"; 
+                    return RedirectToAction("Index");            //Redirect to Client list
                 }
             }
             catch (Exception ex)
@@ -215,33 +216,36 @@ namespace Salon_System.Controllers
                     {
                         if (client.Phone != null && PhoneIsValid(client.Phone) == true) //Check if Phone is valid
                         {
-                            var updateClient = new Client() //Add new input data to client record
-                            {
-                                Id = client.Id,
-                                FirstName = client.FirstName,
-                                LastName = client.LastName,
-                                Gender = client.Gender,
-                                DateOfBirth = client.DateOfBirth,
-                                Email = client.Email,
-                                Phone = client.Phone,
-                                Address = client.Address,
-                                Suburb = client.Suburb,
-                                City = client.City,
-                                Country = client.Country,
-                                PostCode = client.PostCode
-                            };
+                            Client? c = _context.Client.AsNoTracking().FirstOrDefault(x => x.Id == client.Id);//get the client from the database for comparison
 
-                            if (!client.Equals(updateClient))                       //If the record was changed
+                            if (client != null && c != null && ClientIsTheSame(client, c) == false) //If input data is not the same as original
                             {
-                                _context.Client.Update(updateClient);               //Update client record in database
+                                var updateClient = new Client() //Add input data to new client object
+                                {
+                                    Id = client.Id,
+                                    FirstName = client.FirstName,
+                                    LastName = client.LastName,
+                                    Gender = client.Gender,
+                                    DateOfBirth = client.DateOfBirth,
+                                    Email = client.Email,
+                                    Phone = client.Phone,
+                                    Address = client.Address,
+                                    Suburb = client.Suburb,
+                                    City = client.City,
+                                    Country = client.Country,
+                                    PostCode = client.PostCode
+                                };
+                                _context.Client.Update(updateClient);               //Update client record
                                 _context.SaveChanges();                             //Save changes made to database
                                 TempData["successMessage"] = "Client Updated";      //Show success message
-                                return RedirectToAction("Index");                   //Redirect to client list
+                                return RedirectToAction("Details",new { id = client?.Id });                 
+                                                                                    //Redirect to Client/Details
                             }
                             else
-                            {                                                       //Else if no changes made
+                            {                                                       //Else client is the same
                                 TempData["sameStateMessage"] = "No changes made";   //Show same state message
-                                return RedirectToAction("Index");                   //Redirect to client list
+                                return RedirectToAction("Details", new { id = client?.Id });                
+                                                                                    //Redirect to Client/Details
                             }
                         }
                         else
@@ -258,19 +262,31 @@ namespace Salon_System.Controllers
                 }
                 else
                 {
-                    TempData["errorMessage"] = "Invalid Form"; //If Model invalid show error
+                    TempData["errorMessage"] = "Invalid Form"; //If Model invalid show error alert
                     return View();
                 }
             }
             catch (Exception ex)
             {
-                TempData["errorMessage"] = ex.Message;
+                TempData["errorMessage"] = ex.Message; //If other problems show error alert
                 return View();
             }
         }
 
+        public bool ClientIsTheSame(Client client, Client c )
+        {
+            if (c?.FirstName == client?.FirstName && c?.LastName == client?.LastName && c?.Gender == client?.Gender
+            && c?.DateOfBirth == client?.DateOfBirth && c?.Email == client?.Email
+            && c?.Phone == client?.Phone && c?.Address == client?.Address && c?.Suburb == client?.Suburb
+            && c?.City == client?.City && c?.Country == client?.Country && c?.PostCode == client?.PostCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
         //---------------------------------------------------------------------------------------------------------------
-        //DELETE METHOD (Added the Delete operations in the Details View)
+        //DELETE METHOD (Delete operations are coded in the Details View)
         //---------------------------------------------------------------------------------------------------------------
 
         [HttpPost]
@@ -280,10 +296,10 @@ namespace Salon_System.Controllers
             {
                 var client = _context.Client.SingleOrDefault(x => x.Id == cli.Id); //Find client record in database by id
 
-                if (client != null)     //If record exists
+                if (client != null)                                 //If record exists
                 {
-                    _context.Client.Remove(client);     //Remove Client from database //Note: Add to Archive instead
-                    _context.SaveChanges();             //Save changes
+                    _context.Client.Remove(client);                 //Remove Client from database //Note: Add to Archive instead
+                    _context.SaveChanges();                         //Save changes
                     TempData["successMessage"] = "Client Removed";  //Show success message
                     return RedirectToAction("Index");               //Return to Client/Index
                 }
@@ -301,23 +317,27 @@ namespace Salon_System.Controllers
             }
         }
 
+
         //---------------------------------------------------------------------------------------------------------------
         //VALIDATION METHODS
         //---------------------------------------------------------------------------------------------------------------
 
-
         public bool EmailIsValid(string email) //Checks if email is valid using Regular Expression
         {
-            //Check the email has 1 @ symbol followed by a fullstop with at least 1 character before and after each symbol. No whitespace and specified domains allowed.
+            //Check email has 1 @ symbol followed by a fullstop with at least 1 character before and after each symbol. No whitespace and specified domains allowed.
             string pattern = @"^[^@\s]+@[^@\s]+\.(com|co.nz|ac.nz|org.nz|govt.nz|co.au|co.uk|net)$";
 
-             return Regex.IsMatch(email, pattern); //returns true if email matches pattern else false
+            //Should add more validations
+
+            return Regex.IsMatch(email, pattern); //returns true if email matches pattern else false
         }
 
         public bool PhoneIsValid(string phone)  //Checks if phone is valid using Regular Expression
         {
-            //Basic validation - checks the input starts with 0 followed by 8-11 digits //Need to add other validations such as area codes etc.
+            //Basic validation - checks the input starts with 0 followed by 8-11 digits
             string pattern =@"^0\d{8,11}$";
+
+            //Should add more validations
 
             return Regex.IsMatch(phone, pattern); //returns true if phone matches pattern else false
         }
