784dbd24551b9940d1e70c115cc9f78ecc4d1767
kimwatene@gmail.com <kimwatene@gmail.com>
2023-06-08 16:15:01 +1200

    Latest

---
+++ Salon_System/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace Salon_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
